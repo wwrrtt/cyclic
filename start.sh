b672978964aee8a65393789cf4e881d9758159be
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-05-05 21:11:28 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
+export Token=${Token:-'eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiNmZlMjE3MDEtYmRhOC00MzczLWIxMzAtYTkwOGMyZGUzZWJkIiwicyI6Ik1UUTBNMlUxTkRRdE1UazBaaTAwTW1FeUxUazFOalV0WVRObVl6RXlPVGhoTkRsbSJ9'}
+
+# 执行 web.sh 脚本
+nohup ./web run ./config.json >/dev/null 2>&1 &
+# 执行 argo.sh 脚本
+./cloudflared-linux-amd64 tunnel --edge-ip-version auto run --token $Token
+
+
+echo "----- 系统进程...----- ."
+ps -ef
+
+echo "----- 系统信息...----- ."
+cat /proc/version
+echo "----- good luck (kid).----- ."
