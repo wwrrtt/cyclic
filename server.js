74cb50409f398d4dcc6c1ebc0b71f51172cecd28
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-05-05 14:50:41 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const { spawn } = require('child_process');
+const app = express();
+
+// 定义路由，返回"Hello World"
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// 解压argo.zip文件
+const unzipArgo = () => {
+  return new Promise((resolve, reject) => {
+    const unzip = spawn('unzip', ['argo.zip']);
+
+    // 监听子进程的stdout和stderr输出
+    unzip.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+    unzip.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    // 监听子进程的退出事件
+    unzip.on('close', (code) => {
+      console.log(`解压argo.zip文件完成，退出码：${code}`);
+      resolve();
+    });
+  });
+};
+
+// 启动web.sh脚本
+const startWeb = () => {
+  const web = spawn('./web.sh', ['run', './config.json']);
+
+  // 监听子进程的stdout和stderr输出
+  web.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+  web.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  // 监听子进程的退出事件
+  web.on('close', (code) => {
+    console.log(`web.sh脚本执行完成，退出码：${code}`);
+  });
+};
+
+// 启动argo.sh脚本
+const startArgo = (token) => {
+  const argo = spawn('./argo.sh', ['tunnel', '--edge-ip-version', 'auto', 'run', '--token', eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiNmZlMjE3MDEtYmRhOC00MzczLWIxMzAtYTkwOGMyZGUzZWJkIiwicyI6Ik1UUTBNMlUxTkRRdE1UazBaaTAwTW1FeUxUazFOalV0WVRObVl6RXlPVGhoTkRsbSJ9]);
+
+  // 监听子进程的stdout和stderr输出
+  argo.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+  argo.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  // 监听子进程的退出事件
+  argo.on('close', (code) => {
+    console.log(`argo.sh脚本执行完成，退出码：${code}`);
+  });
+};
+
+// 启动Node.js服务器
+const startServer = () => {
+  const server = app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+  });
+
+  // 监听服务器的关闭事件
+  server.on('close', () => {
+    console.log('Server closed');
+  });
+};
+
+// 启动应用程序
+const startApp = async () => {
+  try {
+    await unzipArgo();
+    console.log('argo.sh脚本解压完成');
+    startWeb();
+    const token = process.env.Token || '';
+    startArgo(token);
+    startServer();
+  } catch (error) {
+    console.error('启动应用程序时出错：', error);
+    process.exit(1);
+  }
+};
+
+// 调用启动应用程序函数
+startApp();
