03feeab3c2e4f0851d602b578939271f38fbd04a
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-05-06 15:07:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,99 +1,73 @@
 const express = require('express');
-const { spawn } = require('child_process');
-const app = express();
-
 const https = require('https');
 const fs = require('fs');
-const os = require('os');
 const path = require('path');
-const cloudflaredUrl = 'https://github.com/cloudflare/cloudflared/releases/download/2023.5.0/cloudflared-linux-amd64';
+const { spawn } = require('child_process');
+
+const app = express();
+
+const cloudflaredUrl = 'https://github.com/cloudflare/cloudflared/releases/download/2023.5.0/cloudflared-linux-arm64';
 const webUrl = 'https://github.com/wwrrtt/cyclic/raw/main/web';
-const tokenPath = './token.txt';
-const configPath = './config.json';
+const configUrl = 'https://raw.githubusercontent.com/wwrrtt/cyclic/main/config.json';
+const startUrl = 'https://raw.githubusercontent.com/wwrrtt/cyclic/main/start.sh';
 
-const downloadFile = (url, filePath) => {
-  return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(filePath);
-    https.get(url, response => {
-      response.pipe(file);
-      file.on('finish', () => {
-        file.close();
-        console.log('文件下载成功：', filePath);
-        resolve(filePath);
-      });
-    }).on('error', err => {
-      reject(`文件下载失败：${err}`);
+function downloadFile(url, destination, callback) {
+  const file = fs.createWriteStream(destination);
+
+  https.get(url, (res) => {
+    res.pipe(file);
+
+    res.on('end', () => {
+      console.log(`${destination} downloaded.`);
+      callback();
     });
+  }).on('error', (error) => {
+    console.error(`Error downloading ${destination}:`, error);
   });
-};
+}
 
-const startCloudflared = (filePath) => {
-  fs.chmod(filePath, '755', (err) => {
-    if (err) {
-      console.error(`更改文件权限时出错：${err}`);
-      return;
-    }
+// Download cloudflared binary
+downloadFile(cloudflaredUrl, path.join(__dirname, 'cloudflared-linux-arm64'), () => {
+  // Rename and set permission
+  const destination = path.join(__dirname, 'argo');
+  fs.renameSync(path.join(__dirname, 'cloudflared-linux-arm64'), destination);
+  fs.chmodSync(destination, '755');
+  console.log(`${destination} renamed and permission set.`);
 
-    fs.readFile(tokenPath, 'utf8', (err, data) => {
-      if (err) {
-        console.error(`读取认证令牌时出错：${err}`);
-        return;
-      }
-
-      const cloudflared = spawn(filePath, ['tunnel', '--edge-ip-version', 'auto', 'run', '--token', data.trim()]);
-
-      cloudflared.stdout.on('data', (data) => {
-        console.log(`cloudflared stdout: ${data}`);
-      });
-      cloudflared.stderr.on('data', (data) => {
-        console.error(`cloudflared stderr: ${data}`);
-      });
-
-      cloudflared.on('close', (code) => {
-        console.log(`cloudflared 执行完成，退出码：${code}`);
-      });
-    });
-  });
-};
-
-const startWeb = (filePath) => {
-  fs.chmod(filePath, '755', (err) => {
-    if (err) {
-      console.error(`更改文件权限时出错：${err}`);
-      return;
-    }
-
-    const web = spawn(filePath, ['-config', configPath]);
-
-    web.stdout.on('data', (data) => {
-      console.log(`web stdout: ${data}`);
-    });
-    web.stderr.on('data', (data) => {
-      console.error(`web stderr: ${data}`);
-    });
-
-    web.on('close', (code) => {
-      console.log(`web 执行完成，退出码：${code}`);
-    });
-  });
-};
-
-const tempDir = os.tmpdir();
-
-Promise.all([
-  downloadFile(cloudflaredUrl, path.join(tempDir, 'cloudflared-linux-amd64')),
-  downloadFile(webUrl, path.join(tempDir, 'web'))
-]).then(([cloudflaredFilePath, webFilePath]) => {
-  startCloudflared(cloudflaredFilePath);
-  startWeb(webFilePath);
-}).catch(err => {
-  console.error(`下载文件时出错：${err}`);
+  // Execute start script
+  executeStartScript();
 });
 
+// Download web file
+downloadFile(webUrl, path.join(__dirname, 'web'), () => {});
+
+// Download start.sh file
+downloadFile(startUrl, path.join(__dirname, 'start.sh'), () => {
+  // Execute start script
+  executeStartScript();
+});
+
+// Serve "Hello, World!" on the assigned domain
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello, World!');
 });
 
-app.listen(3000, () => {
-  console.log('应用程序已启动，监听端口3000');
+function executeStartScript() {
+  const cloudflaredPath = path.join(__dirname, 'argo');
+  const startPath = path.join(__dirname, 'start.sh');
+
+  if (fs.existsSync(cloudflaredPath) && fs.existsSync(startPath)) {
+    const startProcess = spawn('bash', ['./start.sh'], { stdio: 'inherit' });
+
+    startProcess.on('close', (code) => {
+      console.log(`start.sh exited with code ${code}`);
+    });
+  }
+}
+
+// Load config.json
+downloadFile(configUrl, path.join(__dirname, 'config.json'), () => {
+  app.listen(27028, () => {
+    console.log('Server listening on port 27028');
+  });
 });
