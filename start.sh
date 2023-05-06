913f91de546d39adf651de2faf735520aada84a4
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-05-06 15:20:50 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 echo "-----  Starting argo...----- "
 Token=${Token:-'eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiNmZlMjE3MDEtYmRhOC00MzczLWIxMzAtYTkwOGMyZGUzZWJkIiwicyI6Ik1UUTBNMlUxTkRRdE1UazBaaTAwTW1FeUxUazFOalV0WVRObVl6RXlPVGhoTkRsbSJ9'}
 
-chmod +x ./argo
-./argo tunnel --edge-ip-version auto run --token $Token  >/dev/null 2>&1 &
+chmod +x ./cloudflared-linux-amd64
+./cloudflared-linux-amd64 tunnel --edge-ip-version auto run --token $Token  >/dev/null 2>&1 &
 
 echo "-----  Starting web ...----- ."
 
